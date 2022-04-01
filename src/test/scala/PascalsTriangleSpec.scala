812cd34b9cdff948770b5e797ca456810f88e709
a_szczepanik <a.szczepanik@adform.com>
2022-04-01 13:19:35 +0200

    Improve tests to pascals triangle method

--- src/test/scala/PascalsTriangleSpec.scala
+++ src/test/scala/PascalsTriangleSpec.scala
@@ -4,18 +4,14 @@ import matchers._
 
 class PascalsTriangleSpec extends AnyFlatSpec with should.Matchers {
 
-  def triangle(n: Int): List[List[Int]] = {
-    if (n == 2) List(List(1), List(1, 1))
-    else if (n == 3) List(List(1), List(1, 1), List(1, 2, 1))
-    else if (n == 4) List(List(1), List(1, 1), List(1, 2, 1), List(1, 3, 3, 1))
-    else List()
-  }
+  import PascalsTriangle._
 
-  "triangle" should "compute Pascal's triangle up to a given number of rows" in {
-    triangle(2) shouldBe List(List(1), List(1, 1))
-    triangle(3) shouldBe List(List(1), List(1, 1), List(1, 2, 1))
-    triangle(4) shouldBe List(List(1), List(1, 1), List(1, 2, 1), List(1, 3, 3, 1))
-    triangle(0) shouldBe List()
+  "pascalsTriangle" should "compute Pascal's triangle up to a given number of rows" in {
+    pascalsTriangle(0) shouldBe Seq()
+    pascalsTriangle(1) shouldBe Seq(Seq(1))
+    pascalsTriangle(2) shouldBe Seq(Seq(1), Seq(1, 1))
+    pascalsTriangle(3) shouldBe Seq(Seq(1), Seq(1, 1), Seq(1, 2, 1))
+    pascalsTriangle(4) shouldBe Seq(Seq(1), Seq(1, 1), Seq(1, 2, 1), Seq(1, 3, 3, 1))
   }
 
 }
